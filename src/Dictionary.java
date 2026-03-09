d91d661a046f398a608339aeb46c55b0890987d1
Simon Stensgaard <STcena1@hotmail.com>
2026-03-09 12:01:34 +0100

    Started working on .insert() method

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -2,12 +2,17 @@ public class Dictionary<K, V> {
     // Internal variables
     private int size;
     private int capacity;
+    private double threshold;
     private Object[] storage;
 
     // Default constructor -> User can specify the internal capacity.
-    public Dictionary(int capacity) {
+    public Dictionary(int capacity, double threshold) {
         if (capacity <= 0) {
-            throw new IllegalArgumentException("Capacity must be represented by a positive integer")
+            throw new IllegalArgumentException("Capacity must be represented by a positive integer");
+        }
+
+        if (threshold <= 0.0 || threshold > 1.0) {
+            throw new IllegalArgumentException("Threshold must be between 0.0 and 1.0");
         }
 
         this.size = 0;
@@ -19,6 +24,7 @@ public class Dictionary<K, V> {
     public Dictionary() {
         this.size = 0;
         this.capacity = 100;
+        this.threshold = 0.5;
         this.storage = initilizeStorage(100);
     }
 
@@ -36,8 +42,12 @@ public class Dictionary<K, V> {
     }
 
     // Internal helper-method for calculating current Load Factor.
-    public double loadFactor() {
+    private double loadFactor() {
         return (double) this.size / this.capacity;
+    }
+
+    private boolean rehashNeeded() {
+        return this.loadFactor() >= this.threshold;
     }
 
     // Internal helper-method for determining if the storage array is currently full.
@@ -45,8 +55,25 @@ public class Dictionary<K, V> {
         return this.size >= this.capacity;
     }
 
+    private int getIndex(int hash) {
+        return hash / this.capacity;
+    }
+
     public boolean insert(K key, V value) {
-        // TODO!
+        if (this.rehashNeeded()) {
+            this.rehash();
+        }
+
+        int hashCode = key.hashCode();
+        int index = this.getIndex(hashCode);
+
+        for (int i = index; i < storage.length; i++) {
+            Entry<K, V> entry = (Entry<K, V>) this.storage[i];
+            if (entry.getStatus() == Placeholder.Occupied) {
+
+            }
+        }
+
     }
 
     public Entry<K, V> remove(K key) {
